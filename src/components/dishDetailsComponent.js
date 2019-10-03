d8056e3ca880f4a479f621721ba6dd6d28ac14cf
Priyanshi Gupta <priyanshigupta997@gmail.com>
2019-10-03 23:47:37 +0530

    MainComponent

--- src/components/dishDetailsComponent.js
+++ src/components/dishDetailsComponent.js
@@ -11,38 +11,50 @@ class DishSelected extends Component{
             commentArray.map((dishComments)=>{
                 return(
                 <div  key={dishComments.id}>
-                        {dishComments.comment} <br></br>
-                        --{dishComments.author} , {dishComments.date}
+                    {dishComments.comment} <br></br>
+                    --{dishComments.author} , {new Intl.DateTimeFormat('en-US', {year:'numeric', month: 'short', day:'2-digit'}).format(new Date(Date.parse(dishComments.date)))}
                 </div>
                 );
             }
-            
         )
         );
     } 
-        return(
-            <div className="row">
-            <div className="col-12 col-md-5 m-1">
-            <Card>
-                <CardImg src={this.props.dish.image} alt={this.props.dish.name}></CardImg>
-                <CardBody>
-                    <CardTitle>{this.props.dish.name}</CardTitle>
-                    <CardText>{this.props.dish.description}</CardText>
-                </CardBody> 
-                
-            </Card>
-            </div>
 
-            <div className="col-md-5 m-1">
-                <h5>Comments</h5>
-                <ul className="list-unstyled">
-                    {renderComments(this.props.dish.comments)}
-                </ul>
+    return(
+        <div className="container">
+            <div className="row">
+                {renderDish(this.props.dish)}
             </div>
+        </div>
+    );
+
+    function renderDish(dish){
+        if(dish!=null){
+            return(
+                <div className="row">
+                <div className="col-12 col-md-5 m-1">
+                <Card>
+                   <CardImg width="100%" src={dish.image} alt={dish.name}></CardImg>
+                   <CardBody>
+                     <CardTitle>{dish.name}</CardTitle>
+                     <CardText>{dish.description}</CardText>
+                   </CardBody> 
+                </Card>
+                </div>
+
+                <div className="col-md-5 m-1">
+                    <h5>Comments</h5>
+                    {renderComments(dish.comments)}
+                </div>
             </div>
-        )
+            )
+        }
+        else{
+            return(
+                <div></div>
+            )
+        }
     }
 }
-
-
+}
 export default DishSelected;
