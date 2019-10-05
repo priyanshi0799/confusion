0c9d62c0a7ffd9487027fb771cd4ab36c3a4f78d
Priyanshi Gupta <priyanshigupta997@gmail.com>
2019-10-05 12:40:09 +0530

    Home Page

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,6 +3,9 @@ import React, {Component} from 'react';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Contact from './ContactComponent';
@@ -13,14 +16,19 @@ class Main extends Component {
   constructor(props){
     super(props);
     this.state = {
-      dishes: DISHES
+      dishes: DISHES,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS
     };
   }
 
   render(){
     const HomePage = ()=>{
       return(
-        <Home/>
+        <Home dish={this.state.dishes.filter((dish)=> dish.featured)[0]}
+        promotion={this.state.promotions.filter((promotion)=> promotion.featured)[0]}
+        leader={this.state.leaders.filter((leader)=> leader.featured)[0]}/>
       );
     }
   return (
